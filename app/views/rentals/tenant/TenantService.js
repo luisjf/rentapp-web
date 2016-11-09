be05fe393cd86743f7f90ab6158d7186eaa03940
jose.luis <jose.luis@cleteci.com>
2016-11-09 08:54:27 -0400

    including toker param on TenantService for tests

--- app/views/rentals/tenant/TenantService.js
+++ app/views/rentals/tenant/TenantService.js
@@ -8,6 +8,9 @@ function getTenants($resource, API_HOST){
     	  search: {
     	    method: 'GET',
     	    // isArray: false,
+          params: {
+    	    	token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6XC9cL2xvY2FsaG9zdDo4MDAwXC9hcGlcL2F1dGhlbnRpY2F0ZSIsImlhdCI6MTQ3ODY5NTgyNSwiZXhwIjoxNDc4Njk5NDI1LCJuYmYiOjE0Nzg2OTU4MjUsImp0aSI6IjhlNzc1M2EyOTQ1ZGJmMzY3ZDgwYzg0ZTVjNmM4YWQzIn0.kpISXmClJ64jE5OSfTbBCAf_Yb2hWNPfvNao09gyt5Q",
+            }
     	  }
     	});
     	return t;
